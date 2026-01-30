a56af8aa0fc652a3c684211a60bcb598ba1405bb
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-01-30 17:48:25 +0000

    Add solution
    Solution uses if statements, for loops, and array methods 'push' and 'reduce'

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let array = [];
+    if ((num1 < 0) || (num2 < 0) || (Number.isInteger(num1) === false) || 
+    (Number.isInteger(num2) === false) || 
+    (num1 == NaN) || (num2 == NaN)) {
+        return "ERROR";
+    } else if (num2 > num1) {
+        for (i=num1; i <= num2; i++) {
+            array.push(i);
+        }
+    } else if (num1 > num2) {
+        for (i=num2; i <= num1; i++) {
+            array.push(i);
+        }
+    }
+    return array.reduce((total, currentItem) => total+currentItem, 0);
 };
 
 // Do not edit below this line
